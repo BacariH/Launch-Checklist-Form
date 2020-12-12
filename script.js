e29dbdcf6261a6a2291f76cf2e1b9eb99b975527
Grrrdolf <herseyb71@gmail.com>
2020-12-12 14:06:20 -0600

    Done with the assignment

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
-// Write your JavaScript code here!
+const form = document.querySelector('#launchForm');
+const formBtn = document.querySelector('#formSubmit');
+const launchStatus = document.querySelector('#launchStatus');
+const fuelStatus = document.querySelector('#fuelStatus');
+const cargoStatus = document.querySelector('#cargoStatus');
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener('load', () => {
+   form.addEventListener('submit', (e) => {
+      let pilotName = document.querySelector('input[name="pilotName"').value;
+      let coPilotName = document.querySelector('input[name="copilotName"').value;
+      let fuelLevel = parseInt(document.querySelector('input[name="fuelLevel"').value);
+      let cargoMass = parseInt(document.querySelector('input[name="cargoMass"').value);
+      let missionTarget = document.querySelector('#missionTarget');
+      let randomPlanetNumber = Math.floor(Math.random() * Math.floor(7));
+
+      if(pilotName === "" || coPilotName === "" || fuelLevel === "" || cargoMass === ""){
+         alert('Please enter some data below.')
+      } else {
+         
+         let faultyItems = document.querySelector('#faultyItems');
+         let pilotStatus = document.querySelector('#pilotStatus');
+         let coPilotStatus = document.querySelector('#copilotStatus');
+         
+         fetch('https://handlers.education.launchcode.org/static/planets.json')
+         .then(res => res.json())
+         .then(data => {
+            for(let i = 0; i < data.length; i++){
+                  let html = '';
+                  html = `
+                  <h2>Mission Destination</h2>
+                  <ol>
+                     <li>Name: ${data[randomPlanetNumber].name}</li>
+                     <li>Diameter: ${data[randomPlanetNumber].diameter}</li>
+                     <li>Star: ${data[randomPlanetNumber].star}</li>
+                     <li>Distance from Earth: ${data[randomPlanetNumber].distance}</li>
+                     <li>Number of Moons: ${data[randomPlanetNumber].moons}</li>
+                  </ol>
+                  <img src="${data[randomPlanetNumber].image}">
+                  `;
+                  missionTarget.innerHTML = html;
+            }
+         });
+
+
+         pilotStatus.textContent = `${pilotName} is ready for launch.`;
+         coPilotStatus.textContent = `${coPilotName} is ready for launch.`;
+
+         if(fuelLevel < 10000 || cargoMass === ''){
+            faultyItems.style.visibility = 'visible';
+            launchStatus.textContent = "Shuttle not ready for launch.";
+            launchStatus.style.color = 'red';
+            fuelStatus.textContent = 'Fuel is too low to launch.';
+         }else if(cargoMass > 10000 || cargoMass === ''){
+            faultyItems.style.visibility = 'visible';
+            launchStatus.textContent = "Shuttle not ready for launch.";
+            launchStatus.style.color = 'red';
+            cargoStatus.textContent = 'Shuttle too heavy to launch.';
+         } else{
+            launchStatus.textContent = 'Shuttle ready for launch';
+            launchStatus.style.color = 'green';
+            fuelStatus.textContent = 'Fuel level high enough for launch.';
+            cargoStatus.textContent = 'Cargo mass is low enough for launch.';
+         }
+      }
+      
+      e.preventDefault();
+   })
+});
