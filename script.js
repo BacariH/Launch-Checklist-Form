b1bfeb7edb522c14ccd7fc482f094720aadcc09a
Grrrdolf <herseyb71@gmail.com>
2020-12-14 18:59:21 -0600

    Added async/await

--- script.js
+++ script.js
@@ -3,28 +3,14 @@ const formBtn = document.querySelector('#formSubmit');
 const launchStatus = document.querySelector('#launchStatus');
 const fuelStatus = document.querySelector('#fuelStatus');
 const cargoStatus = document.querySelector('#cargoStatus');
+let missionTarget = document.querySelector('#missionTarget');
+let randomPlanetNumber = Math.floor(Math.random() * Math.floor(7));
 
-window.addEventListener('load', () => {
-   form.addEventListener('submit', (e) => {
-      let pilotName = document.querySelector('input[name="pilotName"').value;
-      let coPilotName = document.querySelector('input[name="copilotName"').value;
-      let fuelLevel = parseInt(document.querySelector('input[name="fuelLevel"').value);
-      let cargoMass = parseInt(document.querySelector('input[name="cargoMass"').value);
-      let missionTarget = document.querySelector('#missionTarget');
-      let randomPlanetNumber = Math.floor(Math.random() * Math.floor(7));
-
-      if(pilotName === "" || coPilotName === "" || fuelLevel === "" || cargoMass === ""){
-         alert('Please enter some data below.')
-      } else {
-         
-         let faultyItems = document.querySelector('#faultyItems');
-         let pilotStatus = document.querySelector('#pilotStatus');
-         let coPilotStatus = document.querySelector('#copilotStatus');
-         
-         fetch('https://handlers.education.launchcode.org/static/planets.json')
-         .then(res => res.json())
-         .then(data => {
-            for(let i = 0; i < data.length; i++){
+async function getPlanets() {
+   try{
+      let res = await fetch('https://handlers.education.launchcode.org/static/planets.json');
+      let data = await res.json();
+      for(let i = 0; i < data.length; i++){
                   let html = '';
                   html = `
                   <h2>Mission Destination</h2>
@@ -38,10 +24,31 @@ window.addEventListener('load', () => {
                   <img src="${data[randomPlanetNumber].image}">
                   `;
                   missionTarget.innerHTML = html;
-            }
-         });
+               }
+   } catch(err){
+      console.log(err)
+   }
 
+}
 
+window.addEventListener('load', () => {
+   form.addEventListener('submit', (e) => {
+      let pilotName = document.querySelector('input[name="pilotName"').value;
+      let coPilotName = document.querySelector('input[name="copilotName"').value;
+      let fuelLevel = parseInt(document.querySelector('input[name="fuelLevel"').value);
+      let cargoMass = parseInt(document.querySelector('input[name="cargoMass"').value);
+      
+
+      if(pilotName === "" || coPilotName === "" || fuelLevel === "" || cargoMass === ""){
+         alert('Please enter some data below.')
+      } else {
+         
+         let faultyItems = document.querySelector('#faultyItems');
+         let pilotStatus = document.querySelector('#pilotStatus');
+         let coPilotStatus = document.querySelector('#copilotStatus');
+         
+         getPlanets();
+            
          pilotStatus.textContent = `${pilotName} is ready for launch.`;
          coPilotStatus.textContent = `${coPilotName} is ready for launch.`;
 
